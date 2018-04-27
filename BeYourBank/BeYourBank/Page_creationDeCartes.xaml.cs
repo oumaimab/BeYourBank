c3466eccd55b373474991263c6fbf14d021610f8
oumaimab <oumaima.belahsen212@gmail.com>
2018-04-27 09:29:33 +0200

    modif27Avril

--- BeYourBank/BeYourBank/Page_creationDeCartes.xaml.cs
+++ BeYourBank/BeYourBank/Page_creationDeCartes.xaml.cs
@@ -12,17 +12,73 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.OleDb;
+using System.Data;
+using System.Collections.ObjectModel;
 
 namespace BeYourBank
 {
     /// <summary>
     /// Logique d'interaction pour Page_creationDeCartes.xaml
     /// </summary>
+
     public partial class Page_creationDeCartes : Page
     {
-        public Page_creationDeCartes()
+        private OleDbConnection connection = new OleDbConnection();
+        ObservableCollection<Beneficiaire> listeInit;
+        ObservableCollection<Beneficiaire> listeSelectionnes = new ObservableCollection<Beneficiaire>();
+        public Page_creationDeCartes(string idUser)
         {
             InitializeComponent();
+            lbl_user_id.Content = idUser;
+            listeInit = new ObservableCollection<Beneficiaire>();
+            connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;" + @"Data Source=C:\Users\MYC\Documents\PFE\BeYourBankBD.accdb";
+        }
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            dataGrid_beneficiaires.SelectedItems.Clear();
+            try
+            {
+
+                string sql = "SELECT * FROM Beneficiaire";
+                OleDbDataAdapter dataAdapter = new OleDbDataAdapter(sql, connection);
+                DataTable ds = new DataTable("Beneficiare");
+                connection.Open();
+                dataAdapter.Fill(ds);
+                connection.Close();
+                dataGrid_beneficiaires.ItemsSource = ds.DefaultView;
+                for( int i=0; i<ds.Rows.Count; i++)
+                {
+                    Beneficiaire benef = new Beneficiaire(ds.Rows[i]["noCINBeneficiaire"].ToString(), ds.Rows[i]["nomBeneficiaire"].ToString(), ds.Rows[i]["prenomBeneficiaire"].ToString(), ds.Rows[i]["noTelBeneficiaire"].ToString(), ds.Rows[i]["dateNaissance"].ToString(), ds.Rows[i]["profession"].ToString(), ds.Rows[i]["adresse"].ToString(), ds.Rows[i]["villeResidence"].ToString(), ds.Rows[i]["codePostal"].ToString(), ds.Rows[i]["sex"].ToString(), ds.Rows[i]["titre"].ToString(), ds.Rows[i]["statut"].ToString(), ds.Rows[i]["idUser"].ToString());
+                    listeInit.Add(benef);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur de connection" + ex);
+            }
+        }
+
+        private void dataGrid_beneficiaires_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            btn_creation.IsEnabled = true;
+        }
+
+        private void btn_creation_Click(object sender, RoutedEventArgs e)
+        {
+            if (dataGrid_beneficiaires.SelectedItems.Count > 0)
+            {
+                AddCard ac = new AddCard(lbl_user_id.Content.ToString());
+                for (int i = 0; i < dataGrid_beneficiaires.SelectedItems.Count; i++)
+                {
+                    DataRowView row = (DataRowView)dataGrid_beneficiaires.SelectedItems[i];
+                    ac.lstBox_CIN.Items.Add(row["noCINBeneficiaire"].ToString());
+                    ac.lstBox_selected.Items.Add(row["nomBeneficiaire"].ToString()+" "+ row["prenomBeneficiaire"].ToString());
+                }
+                ac.ShowDialog();
+            }
+
         }
     }
 }
--- BeYourBank/BeYourBank/Page_operations.xaml.cs
+++ BeYourBank/BeYourBank/Page_operations.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.OleDb;
+using System.IO;
 
 namespace BeYourBank
 {
@@ -20,9 +22,81 @@ namespace BeYourBank
     /// </summary>
     public partial class Page_operations : Page
     {
-        public Page_operations()
+        private OleDbConnection connection = new OleDbConnection();
+        public Page_operations(string idUser)
         {
             InitializeComponent();
+            lbl_user.Content = idUser;
+            connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;" + @"Data Source=C:\Users\MYC\Documents\PFE\BeYourBankBD.accdb";
+        }
+
+        private void btn_genererFichier_Click(object sender, RoutedEventArgs e)
+        {
+            string referenceConvention = null;
+            string codeProduit = null;
+            string numCompte = null;
+            string codeCompagnie = null;
+            string nomOrganisme = null;
+            string raisonSociale = null;
+            try
+            {
+                connection.Open();
+                OleDbCommand command = new OleDbCommand();
+                command.Connection = connection;
+                command.CommandText = "select * from Convention where idUser ='" + lbl_user.Content.ToString() +"';";
+                OleDbDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        referenceConvention = reader[0].ToString();
+                        codeProduit = reader[1].ToString();
+                        numCompte = reader[2].ToString();
+                        codeCompagnie = reader[3].ToString();
+                        nomOrganisme = reader[4].ToString();
+                        raisonSociale = reader[5].ToString();
+                    }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur de connection" + ex);
+            }
+
+            string dateTodayDay = null;
+            string dateTodayMonth= null;
+            string dateTodayYear2 = null;
+            string dateTodayFormat = null;
+            string index = "222";
+            if (System.DateTime.Now.Day.ToString().Length == 1)
+            {
+                dateTodayDay = "0" + System.DateTime.Now.Day;
+            }
+            else if(System.DateTime.Now.Day.ToString().Length == 2)
+            {
+                dateTodayDay = System.DateTime.Now.Day.ToString();
+            }
+
+            if (System.DateTime.Now.Month.ToString().Length == 1)
+            {
+                dateTodayMonth = "0" + System.DateTime.Now.Month;
+            }
+            else if (System.DateTime.Now.Day.ToString().Length == 2)
+            {
+                dateTodayMonth = System.DateTime.Now.Month.ToString();
+            }
+
+            dateTodayFormat = dateTodayDay + dateTodayMonth + System.DateTime.Now.Year.ToString();
+            dateTodayYear2 = System.DateTime.Now.Year.ToString().ElementAt(2).ToString() + System.DateTime.Now.Year.ToString().ElementAt(3).ToString();
+            string fichier = "PREP_CONVENTION000000."+ dateTodayYear2 + dateTodayMonth + dateTodayDay + index +".txt";
+            using (StreamWriter writer = new StreamWriter(fichier, true))
+            {
+                writer.Write("7FH"+codeCompagnie+"00001"+dateTodayFormat+System.DateTime.Now.Hour+ System.DateTime.Now.Minute + System.DateTime.Now.Second + index);
+            }
+
+
+
+
+            MessageBox.Show("Le fichier a bien été créé dans l'emplacement spécifié!", "ok", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
