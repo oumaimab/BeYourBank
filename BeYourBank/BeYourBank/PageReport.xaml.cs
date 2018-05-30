785a959a4dff1743f9a7c96f4ff78bea8ae43ccf
oumaimab <oumaima.belahsen212@gmail.com>
2018-05-30 14:23:34 +0100

    rapportDONE

--- BeYourBank/BeYourBank/PageReport.xaml.cs
+++ BeYourBank/BeYourBank/PageReport.xaml.cs
@@ -17,6 +17,7 @@ using iTextSharp.text;
 using iTextSharp.text.pdf;
 using System.Data.OleDb;
 using System.Configuration;
+using System.Data;
 
 namespace BeYourBank
 {
@@ -26,7 +27,9 @@ namespace BeYourBank
     public partial class PageReport : Page
     {
         private string idUtilisateur;
+
         private OleDbConnection connection = new OleDbConnection();
+        DataTable ds = new DataTable("Beneficiare_table");
         public PageReport(string idUser)
         {
             InitializeComponent();
@@ -42,6 +45,9 @@ namespace BeYourBank
             try
             {
                 connection.Open();
+                string sql = "SELECT dateOperation as [Date Opération] , TypeOperation as [Type Opération] , count(idOperation) as [Nombre de bénéficaires] FROM Operations group by dateOperation , TypeOperation order by dateOperation desc ";
+                OleDbDataAdapter dataAdapter = new OleDbDataAdapter(sql, connection);
+                dataAdapter.Fill(ds);
                 OleDbCommand command = new OleDbCommand();
                 command.Connection = connection;
                 command.CommandText = "SELECT * FROM Convention WHERE refConvention= (SELECT idConvention FROM Utilisateurs WHERE noCINUser='" + idUtilisateur + "');";
@@ -62,24 +68,60 @@ namespace BeYourBank
             Document doc = new Document(iTextSharp.text.PageSize.A4, 20, 20, 40, 30);
             PdfWriter wri = PdfWriter.GetInstance(doc, new FileStream("Rapport_BeYourBank.pdf", FileMode.Create));
             doc.Open();
-
-            Chunk c1 = new Chunk("Rapport", FontFactory.GetFont("Helvetica"));
+            
+            Chunk c1 = new Chunk("Rapport \n", FontFactory.GetFont("Candara"));
             c1.Font.Color = new iTextSharp.text.BaseColor(0, 0, 20);
             c1.Font.SetStyle(0);
-            c1.Font.Size = 20;
+            c1.Font.Size = 25;
 
-            Chunk c2 = new Chunk("--- BE YOUR BANK ---", FontFactory.GetFont("Verdana"));
+            Chunk c2 = new Chunk("BeYourBank", FontFactory.GetFont("Candara"));
             c2.Font.Color = new iTextSharp.text.BaseColor(0, 0, 20);
             c2.Font.SetStyle(0);
-            c2.Font.Size = 15;
+            c2.Font.Size = 20;
 
-            Chunk c3 = new Chunk("--- BE YOUR BANK ---", FontFactory.GetFont("Verdana"));
-            c3.Font.Color = new iTextSharp.text.BaseColor(0, 0, 20);
-            c3.Font.SetStyle(0);
-            c3.Font.Size = 15;
+            //auth
+            Chunk auth = new Chunk("Date de création : " + DateTime.Now.ToShortDateString(), FontFactory.GetFont("Candara"));
+            BaseFont bFont = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+            Font fontAuth = new Font(bFont, 8, 2, BaseColor.GRAY);
+            iTextSharp.text.Paragraph authP = new iTextSharp.text.Paragraph(auth);
+            authP.Alignment = Element.ALIGN_RIGHT;
+            doc.Add(authP);
 
-            iTextSharp.text.Paragraph p1 = new iTextSharp.text.Paragraph("Hello ! premier paragraphe");
+            //pdf header
+            iTextSharp.text.Paragraph p1 = new iTextSharp.text.Paragraph(c1);
+            p1.Alignment = Element.ALIGN_CENTER;
+            p1.Add(c2);
             doc.Add(p1);
+
+            //ajouter une ligne de séparation
+            iTextSharp.text.Paragraph p = new iTextSharp.text.Paragraph(new Chunk(new iTextSharp.text.pdf.draw.LineSeparator(0.0F, 100.0F, BaseColor.BLACK, Element.ALIGN_MIDDLE, 0)));
+            doc.Add(p);
+
+            doc.Add(new Chunk("\n", FontFactory.GetFont("Verdana")));
+
+            //table d'historique
+            PdfPTable pdfTableHist = new PdfPTable(ds.Columns.Count);
+
+            //table header
+            BaseFont bfColumunHeader = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+            Font ftnColumnHeader = new Font(bfColumunHeader, 10, 1, BaseColor.WHITE);
+            for (int i = 0; i < ds.Columns.Count; i++)
+            {
+                PdfPCell cell = new PdfPCell();
+                cell.BackgroundColor = BaseColor.GRAY;
+                cell.AddElement(new Chunk(ds.Columns[i].ColumnName.ToUpper(), ftnColumnHeader));
+                pdfTableHist.AddCell(cell);
+            }
+            //table cellules
+            for (int i = 0; i < ds.Rows.Count; i++)
+            {
+                pdfTableHist.AddCell(ds.Rows[i][0].ToString().Substring(0, 10));
+                for (int j = 0; j < ds.Columns.Count- 1 ; j++)
+                {   
+                    pdfTableHist.AddCell(ds.Rows[i][j+1].ToString());
+                }
+            }
+            doc.Add(pdfTableHist);
             doc.Close();
             MessageBox.Show("PDF CREATED !");
         }
